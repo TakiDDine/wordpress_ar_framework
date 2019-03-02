5cc48ecd02655a0155685ac0c6228ff92cc0950d
TakiDDine <takiddine.job@gmail.com>
2019-03-02 18:44:22 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -89,7 +89,12 @@ require_once THEME_DIR. '/core/html-compressor.php';
 //require_once THEME_DIR. '/core/libraries/init.php';
 
 
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
 
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 
 
